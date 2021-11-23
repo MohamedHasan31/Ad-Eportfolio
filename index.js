c3487c26372cd5b7df2721d30d282540d3630cc8
MohamedHasan31 <94693045+MohamedHasan31@users.noreply.github.com>
2021-11-23 10:05:16 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+let contrastToggle = false
+const scaleFactor =1 / 20;
+
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX / 20;
+    const y = event.clientY / 20;
+
+    for (let i = 0; i < shapes.length; ++i) {
+        const isOdd = i % 2 !== 0;
+        const boolInt = isOdd ? -1 : 1;
+        shapes[i].style.transform =`translate(${x * boolInt}px, ${y * boolInt}px)`
+    }
+}
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+        document.body.classList += " dark-theme"
+    }
+    else {
+        document.body.classList.remove("dark-theme")
+    }
+}
+
+
+function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading')
+    const success = document.querySelector('.modal__overlay--success')
+    loading.classList += " modal__overlay--visible";
+    emailjs
+        .sendForm(
+            'service_kz9x1xt',
+            'template_tkfwm7n',
+            event.target,
+            'user_2aZEzFy28tzSRSi4cKsF9'
+        ) .then(() => {
+            loading.classList.remove("modal__overlay--visible");
+            success.classList += " modal__overlay--visible";
+        }) .catch(() => {
+            loading.classList.remove("modal__overlay--visible");
+            alert(
+                "The email serviceis temporarily unavailable. Please contact me at Mohamedhassan3193@gmail.com"
+            );
+        })
+  
+}
+let isModalOpen = false;
+function toggleModal() {
+    //toggle modal
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove ("modal--open")
+    }
+    isModalOpen = true;
+    document.body.classList += " modal--open"
+}
